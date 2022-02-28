f871e209f20eee872ad9d2d7a36070e327f86ad7
Priyanka Jaiswal <jaiswal.priyanka181294@gmail.com>
2022-02-28 14:43:18 +0530

    React Basics & Working with Components - Writing more complex JSX code

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>;
+    return (
+        <div>
+            <div>February 28th, 2022</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$294.67</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
