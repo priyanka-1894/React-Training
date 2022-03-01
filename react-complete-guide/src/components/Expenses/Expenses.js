eab2d77d878eb1f815914e165e1ff19f426f0266
Priyanka Jaiswal <jaiswal.priyanka181294@gmail.com>
2022-03-01 17:04:25 +0530

    React State & Working with Events - Adding Expenses Filter Component & Controlled vs Uncontrolled Components & Stateless vs Stateful Components

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,19 +1,23 @@
+import { useState } from "react";
+
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
-
 import Card from '../UI/Card';
 
 import './Expenses.css';
 
 function Expenses(props) {
-    const filterExpenseHandler = (filter) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = (filter) => {
+        setFilteredYear(filter);
         props.filterExpense(filter);
     }
 
     return (
         <div>
             <Card className="expenses">
-                <ExpensesFilter filterExpense={ filterExpenseHandler } />
+                <ExpensesFilter selected={ filteredYear } filterExpense={ filterChangeHandler } />
                 <ExpenseItem title={ props.expenses[0].title } amount={ props.expenses[0].amount } date={ props.expenses[0].date } />
                 <ExpenseItem title={ props.expenses[1].title } amount={ props.expenses[1].amount } date={ props.expenses[1].date } />
                 <ExpenseItem title={ props.expenses[2].title } amount={ props.expenses[2].amount } date={ props.expenses[2].date } />
