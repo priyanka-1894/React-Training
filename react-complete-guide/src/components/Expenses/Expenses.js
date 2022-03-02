b0e382d92ab408d41f98d0e3eed0417b0b303005
Priyanka Jaiswal <jaiswal.priyanka181294@gmail.com>
2022-03-02 13:42:07 +0530

    Rendering Lists & Conditional Content: Rendering Lists of Data, Using Stateful Lists, Understanding 'Keys'

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -11,17 +11,17 @@ function Expenses(props) {
 
     const filterChangeHandler = (filter) => {
         setFilteredYear(filter);
-        props.filterExpense(filter);
     }
+
+    const filteredExpenses = props.expenses.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
 
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={ filteredYear } filterExpense={ filterChangeHandler } />
-                <ExpenseItem title={ props.expenses[0].title } amount={ props.expenses[0].amount } date={ props.expenses[0].date } />
-                <ExpenseItem title={ props.expenses[1].title } amount={ props.expenses[1].amount } date={ props.expenses[1].date } />
-                <ExpenseItem title={ props.expenses[2].title } amount={ props.expenses[2].amount } date={ props.expenses[2].date } />
-                <ExpenseItem title={ props.expenses[3].title } amount={ props.expenses[3].amount } date={ props.expenses[3].date } />
+                { filteredExpenses.map(expense => <ExpenseItem key={ expense.id } title={ expense.title } amount={ expense.amount } date={ expense.date } />) }
             </Card>
         </div>
     );
